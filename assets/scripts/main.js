963f37ccc44a546824b5de9c1f36172bed0d4d6e
PrathamMittal's MacBook VSCode <pmittal@ucsd.edu>
2022-11-14 22:32:59 -0800

    explore check

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,29 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ("serviceWorker" in navigator) {
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', async () => {
+      try {
+        // Steps B3-B6 will be *inside* the event listener's function created in B2
+        // B3. TODO - Register './sw.js' as a service worker (The MDN article
+        //            "Using Service Workers" will help you here)
+        const registration = await navigator.serviceWorker.register("./sw.js");
+        // B4. TODO - Once the service worker has been successfully registered, console
+        //            log that it was successful.
+        if (registration.active) {
+          console.log("Service Worker Registered");
+        }
+      } catch (error) {
+        // B5. TODO - In the event that the service worker registration fails, console
+        //            log that it has failed.
+        console.log("Registration failed");
+        // STEPS B6 ONWARDS WILL BE IN /sw.js
+      }
+  })
+}
+else {
+  return;
 }
 
 /**
@@ -70,6 +84,9 @@ async function getRecipes() {
   //            If there are recipes, return them.
   if(localStorage.getItem('recipes') != null) {
     return JSON.parse(localStorage.getItem('recipes'));
+  }
+  else {
+    return;
   }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
--- sw.js
+++ sw.js
@@ -34,7 +34,20 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    return catch.match(event.request).then((catchedResponse) => {
+      // B8. TODO - If the request is in the cache, return with the cached version.
+      //            Otherwise fetch the resource, add it to the cache, and return
+      //            network response.
+      if (catchedResponse) {
+        return catchedResponse;
+      }
+      else {
+        return fetch(event.request).then((fetchedResponse) => {
+          cache.put(event.request, fetchedResponse.clone());
+          return fetchedResponse;
+        });
+      }
+    });
+  }));
 });
